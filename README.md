29c9a9bea6436d556949446a38504a86551d361f
nickpizzoni <nickpizzoni@outlook.com>
2024-03-09 02:42:38 +0000

    feat: adicionado  como formatador de codigo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews-clone
+
 Projeto do https://curso.dev do Filipe Deschamps para construção do zero do site https://tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 function Home() {
-    return (
-        <div>
-            <h1>Te amo infinito meu amor!</h1>
-            <p>❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️</p>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Te amo infinito meu amor!</h1>
+      <p>❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️</p>
+    </div>
+  );
 }
 
 export default Home;
