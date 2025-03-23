382bfef746c638da4e0afe16bc44a0eeb2f52173
nickpizzoni <nickpizzoni@outlook.com>
2025-03-23 00:48:12 +0000

    feat: add  ao projeto

--- infra/database.js
+++ infra/database.js
@@ -2,10 +2,11 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: "local"
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD
   });
   //Comando assíncrono, precisa ir até o banco para realizar a conexão
   await client.connect();
