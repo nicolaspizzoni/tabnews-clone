5748f6e0d6322b93051a00907294c8b893068672
nickpizzoni <nickpizzoni@outlook.com>
2025-05-17 16:03:48 +0000

    feat: max, opened connections e sql injection teste

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
 import database from "infra/database"
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log("database return ", result.rows)
-  res.status(200).json({ chave: "são acima da média" })
+  const updatedAt = new Date().toISOString();
+
+  const queryServerVersion = await database.query("SHOW server_version;");
+  const serverVersion = queryServerVersion.rows[0].server_version;
+
+  const queryMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnections = queryMaxConnections.rows[0].max_connections;
+
+  const databaseName = req.query.databaseName;
+  const queryOpenedConnections = await database.query(`SELECT count(*)::int FROM pg_stat_activity WHERE datname='${databaseName}';`);
+  const openedConnections = queryOpenedConnections.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: serverVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections
+      }
+    }
+  })
 }
 
 export default status
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,24 @@
 test("GET api/v1/status should return status code 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status")
+  const response = await fetch("http://localhost:3000/api/v1/status?databaseName=local_database")
   expect(response.status).toBe(200)
+
+  const responseBody = await response.json();
+  expect(responseBody).toBeDefined();
+
+  const updatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBe(updatedAt)
+
+  const versionDatabase = responseBody.dependencies.database.version;
+  expect(versionDatabase).toBe('16.0');
+
+  const maxConnections = responseBody.dependencies.database.max_connections;
+  expect(maxConnections).toBe(100);
+
+  const openedConnections = responseBody.dependencies.database.opened_connections;
+  expect(openedConnections).toBe(1);
+})
+
+it("should test for SQL Injection", async () => {
+  //Injeção SQL ao fechar primeira query e lançar nova fazendo o back levar 4s na requisição
+  await fetch("http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4) --")
 })
