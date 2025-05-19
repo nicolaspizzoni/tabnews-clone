518b4b584eeac5cffb3806fffb88ef1e7dfe10de
nickpizzoni <nickpizzoni@outlook.com>
2025-05-19 00:49:58 +0000

    melhorado error handling no arquivo `database.js` no `client.connect`

--- infra/database.js
+++ infra/database.js
@@ -8,14 +8,21 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD
   });
-  //Comando assíncrono, precisa ir até o banco para realizar a conexão
-  await client.connect();
 
   try {
+    //Comando assíncrono, precisa ir até o banco para realizar a conexão
+    await client.connect();
     const res = await client.query(queryObject);
     return res;
   } catch (error) {
-    console.error(error)
+    console.error("dados ambiente: ", {
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      user: process.env.POSTGRES_USER,
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD
+    })
+    throw error
   } finally {
     await client.end();
   }
